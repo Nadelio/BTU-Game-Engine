0a6bf11b52ba10a968a819fb8a5f86ce1c1dc651
Nadelio <carefulsniffle@gmail.com>
2025-02-22 12:58:44 -0600

    Working on Entry point

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,6 @@
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+
 public class Main {
     // use this to initialize the rendering system,
     // add all the threads,
@@ -6,4 +9,18 @@ public class Main {
     // and start the game loop from the initial scene defined in the config file
 
     // render thread, update thread, fixed update executor
+
+    // render -> Thread
+    // update -> Thread
+    // fixed update -> ScheduledExecutorService
+
+    public static void main(String [] args){ // [window config file path, initial scene index]
+        Renderer renderer = new Renderer();
+        Updater updater = new Updater();
+        Thread renderThread = new Thread(renderer, "Render Thread");
+        Thread updateThread = new Thread(updater, "Update Thread");
+        ScheduledExecutorService fixedUpdateExecutor = Executors.newScheduledThreadPool(5);
+
+        configureWindow(args[0]);
+    }
 }

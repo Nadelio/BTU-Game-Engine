25b654cad08e706f299e862eb3faafff9ca0d98d
Nadelio <carefulsniffle@gmail.com>
2025-02-24 19:32:37 -0600

    Renderer docs, refactoring, threads -> scheduled executors

--- src/Main.java
+++ src/Main.java
@@ -19,13 +19,17 @@ public class Main {
     // fixed update -> ScheduledExecutorService
 
     public static void main(String [] args){ // [window config file path, initial scene index]
-        Renderer renderer = new Renderer(); // implements Runnable
-        ComponentUpdater updater = new ComponentUpdater();
+        //? implements Runnable
+        //? needs to have a registry of all the game objects
+        //? needs to keep track of game object layering
+        //? needs to be an infinite loop that renders the game objects
+        //? needs to be able to render the game objects in the correct order and only if they have a sprite component
+        //? possibly implement some sort of batch rendering system? (Renderer1 does game objects 1 - n, Renderer2 does game objects n+1 - m, etc)
+        Renderer renderer = new Renderer(); //! might need to only repaint() since javafx might handle the layering and rendering
+
+        ComponentUpdater componentUpdater = new ComponentUpdater();
         ScriptUpdater scriptUpdater = new ScriptUpdater();
         FixedUpdater fixedUpdater = new FixedUpdater();
-        Thread renderThread = new Thread(renderer, "Render Thread");
-        Thread cUpdateThread = new Thread(updater, "Component Update Thread");
-        Thread sUpdateThread = new Thread(scriptUpdater, "Script Update Thread");
         
         if(args.length == 0) throw new IllegalArgumentException("No window configuration file path provided");
         if(args.length == 1) throw new IllegalArgumentException("No initial scene index provided");
@@ -41,13 +45,27 @@ public class Main {
         
         loadGameScene(args[1]); // loads the initial scene from the command line arg[1]
 
+        ScheduledExecutorService renderExecutorService = Executors.newSingleThreadScheduledExecutor();
+        ScheduledExecutorService cUpdateExecutorService = Executors.newSingleThreadScheduledExecutor();
+        ScheduledExecutorService sUpdateExecutorService = Executors.newSingleThreadScheduledExecutor();
         ScheduledExecutorService fixedUpdateExecutor = Executors.newSingleThreadScheduledExecutor();
-        renderThread.start();
-        fixedUpdateExecutor.scheduleAtFixedRate(fixedUpdater, 0, 20, TimeUnit.MILLISECONDS);
-        cUpdateThread.start();
-        sUpdateThread.start();
 
-        
-        
+        renderExecutorService.schedule(renderer, 0, TimeUnit.MILLISECONDS);
+        fixedUpdateExecutor.scheduleAtFixedRate(fixedUpdater, 0, 20, TimeUnit.MILLISECONDS);
+        cUpdateExecutorService.schedule(componentUpdater, 0, TimeUnit.MILLISECONDS);
+        sUpdateExecutorService.schedule(scriptUpdater, 0, TimeUnit.MILLISECONDS);
+
+    }
+
+    private static void configureWindow(String configFilePath){
+        // lex, parse, and then interpret the window.config file
+        // set the window title, width, height, and other properties
+    }
+
+    private static void loadGameScene(String sceneIndex){
+        int index = Integer.parseInt(sceneIndex);
+        // load the game scene from the scene index
+        // get the Scene object from the scene index
+        // iterate through every top level game object and register them
     }
 }
